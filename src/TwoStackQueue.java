746bff8c91e3d4ec71376e2b8f6e6138e83c1eb7
fengjutian <2640111081@qq.com>
2020-11-02 17:40:08 +0800

    save

---
+++ src/TwoStackQueue.java
@@ -0,0 +1,16 @@
+import java.util.Stack;
+
+public class TwoStackQueue {
+    public Stack<Integer> stackPush;
+    public Stack<Integer> stackPop;
+
+    public TwoStackQueue() {
+        stackPush = new Stack<Integer>();
+        stackPop = new Stack<>();
+    }
+
+    // push 栈向pop栈导入数据
+    private void 
+
+
+}
